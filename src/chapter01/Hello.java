949db855196d8d38c62c2e39ac38f8b77780121b
iknowonder <iknowonder@gmail.com>
2018-01-25 01:58:23 +0900

    chapter 01 finished

---
+++ src/chapter01/Hello.java
@@ -0,0 +1,7 @@
+package chapter01; //packge statement
+
+public class Hello /* class name */{ //class block
+    public static void main(String[] args) { //method block
+        System.out.println("Hello, world!");
+    }
+}
---
+++ src/chapter01/RunStatementExample.java
@@ -0,0 +1,10 @@
+package chapter01;
+
+public class RunStatementExample {
+    public static void main(String[] args){
+        int x = 1; //declare variable x and store 1
+        int y = 2; //declare variable y and store 2
+        int result = x + y; //declare variable result and store x + y
+        System.out.println(x + "+" + y + "="+result); // calling method to output to consol
+    }
+}
