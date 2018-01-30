b087f5aa4dcd5bafab09718f29a7cda3e5563653
KMU\iknowonki <iknowonder@gmail.com>
2018-01-31 03:20:25 +0900

    add VariableScopeExample modify chapter2 README

---
+++ src/chapter02/VariableScopeExample.java
@@ -0,0 +1,10 @@
+package chapter02;
+
+public class VariableScopeExample {
+    public static void main(String[] args) {
+        int v1 = 15;
+        if(v1>10) {
+            int v2 = v1 - 10;
+        }
+    }
+}
