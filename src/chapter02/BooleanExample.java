5a98ce1f8d8665399e7465d6ab0cdd55c62b1b5b
KMU\iknowonki <iknowonder@gmail.com>
2018-02-09 05:01:50 +0900

    add BooleanExample

---
+++ src/chapter02/BooleanExample.java
@@ -0,0 +1,12 @@
+package chapter02;
+
+public class BooleanExample {
+    public static void main(String [] args) {
+        boolean stop = true;
+        if(stop) {
+            System.out.println("Stop!");
+        } else {
+            System.out.println("Start!");
+        }
+    }
+}
